f64f4439d84104396505b090c840398e79c49137
Clara Andrade <113606213+claraandradee@users.noreply.github.com>
2024-08-15 08:16:40 -0300

    feat: update ClientController

--- src/main/java/br/unicamp/cotuca/dpd/foodConnect/infrastructure/cep/ViaCepClient.java
+++ src/main/java/br/unicamp/cotuca/dpd/foodConnect/infrastructure/cep/ViaCepClient.java
@@ -4,8 +4,8 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(url = "https://viacep.com.br/ws/", name = "viacep")
+@FeignClient(url = "https://api.postmon.com.br/v1/cep/", name = "postmon")
 public interface ViaCepClient {
-    @GetMapping("{cep}/json")
+    @GetMapping("{cep}")
     Address fetchAddressByCep(@PathVariable("cep") String cep);
 }
